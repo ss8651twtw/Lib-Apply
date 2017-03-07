70329de7a71f69aaecc5a056fd3113c6482b6f08
ss8651twtw <ss8651twtw@gmail.com>
2017-03-07 18:42:34 +0800

    implement card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { RequestService } from "../_services/request.service"
 
 @Component( {
   selector: 'app-card',
@@ -6,11 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: [ 'card.component.scss' ],
   host: { class: "app-wrapper" }
 } )
-export class CardComponent {
+export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor( private request: RequestService ) { }
+  
+  current_page: number = 0;
+  applydate: string;
 
-  onSubmitted() {
-
+  ngOnInit() {
+    let date = new Date();
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    this.applydate = y + '-' + m + '-' + d;
   }
+
+  prevPage() {
+    this.current_page--;
+  }
+
+  nextPage() {
+    this.current_page++;
+  }
+
+  checkPerson: boolean = false;
+  person() { this.checkPerson = true; }
+  money() { this.checkPerson = false; }
+
+  checkMale: boolean = false;
+  male() { this.checkMale = true; }
+  female() { this.checkMale = false; }
+
+  onSubmitted( form: NgForm ) {
+    let formData = form['value'];
+    formData['time'] = this.applydate;
+    formData['gender'] = this.checkMale ? 'male' : 'female';
+    formData['guarantee'] = this.checkPerson;
+    formData['gmoney'] = !this.checkPerson;
+  }
+
 }
