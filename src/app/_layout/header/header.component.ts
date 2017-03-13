85bc63c301866759950054ff206e39a8d4b4b855
OAlienO <jeffrey6910@gmail.com>
2017-03-13 17:07:54 +0800

    fix header RWD

--- src/app/_layout/header/header.component.ts
+++ src/app/_layout/header/header.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../_services/auth.service";
 import { CookieService } from "../../_services/cookie.service";
+
+declare var $;
 
 @Component( {
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: [ 'header.component.scss' ]
 } )
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   constructor( private cookie: CookieService, private auth: AuthService ) { }
-  
+
+  ngOnInit(){
+    $( document ).ready(function(){});
+    $(".button-collapse").sideNav();
+  }
+
   userLogout() {
-  this.auth.logout({})
-  .subscribe(data => console.log(data));
+    this.auth.logout( {} )
+      .subscribe( data => console.log( data ) );
   }
 }
